626fd698600dc79b76e8fa8ed1edbdf7e18b0982
Salim KHALIL <khalilsalim1@gmail.com>
2026-03-27 09:02:58 +0000

    Initial commit

---
+++ src/main/java/com/ecom/gestionecole/GestionEcoleApplication.java
@@ -0,0 +1,13 @@
+package com.ecom.gestionecole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionEcoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionEcoleApplication.class, args);
+	}
+
+}
